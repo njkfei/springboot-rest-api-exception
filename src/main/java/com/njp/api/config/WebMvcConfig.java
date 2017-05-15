511cd30b63772f7756b870732ae0de685467565d
jpnie <jpnie@rxhui.com>
2017-05-15 15:12:22 +0800

    增加权限验证功能。

---
+++ src/main/java/com/njp/api/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.njp.api.config;
+
+import com.njp.api.security.AccessTokenVerifyInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by niejinping on 2017/5/15.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public AccessTokenVerifyInterceptor tokenVerifyInterceptor() {
+        return new AccessTokenVerifyInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 此处可以从数据库或配置文件里面拉url配置信息
+        registry.addInterceptor(tokenVerifyInterceptor()).addPathPatterns("/api/**");
+        super.addInterceptors(registry);
+    }
+
+}
